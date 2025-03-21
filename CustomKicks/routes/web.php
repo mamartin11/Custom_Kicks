cc64c9898eab8a7aed76144c00babdc8b686a62e
Santiago <santiagord2004@gmail.com>
2025-03-21 11:06:53 -0500

    feat:item_class

--- CustomKicks/routes/web.php
+++ CustomKicks/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/items/add', 'App\Http\Controllers\ItemController@create')->name('items.add');
+Route::get('/items/index', 'App\Http\Controllers\ItemController@index')->name("items.index");
+Route::get('/items/{id}', 'App\Http\Controllers\ItemController@show')->name('items.show');
+Route::post('/items/store', 'App\Http\Controllers\ItemController@store')->name('items.store');
+
+Route::delete('/items/{id}', 'App\Http\Controllers\ItemController@destroy')->name('items.destroy');
