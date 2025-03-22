70f8c79b0d2604d29072d191999822c82760e6c6
Santiago <santiagord2004@gmail.com>
2025-03-22 15:06:55 -0500

    Feat:Add filters to Products

--- CustomKicks/app/Http/Controllers/ProductController.php
+++ CustomKicks/app/Http/Controllers/ProductController.php
@@ -4,15 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(): view
+    public function index(Request $request): view
     {
+        $query = Product::query();
+
+        if ($request->filled('brand')) {
+            $query->where('brand', $request->input('brand'));
+        }
+    
+        if ($request->filled('min_price')) {
+            $query->where('price', '>=', $request->input('min_price'));
+        }
+    
+        if ($request->filled('max_price')) {
+            $query->where('price', '<=', $request->input('max_price'));
+        }
+    
+        if ($request->filled('size')) {
+            $query->where('size', $request->input('size'));
+        }
+    
+        if ($request->filled('quantity')) {
+            $query->where('quantity', '>=', $request->input('quantity'));
+        }
+
+        $brands = Product::select('brand')->distinct()->pluck('brand');
+
         $viewData = [];
         $viewData['title'] = 'Products - Online Store';
         $viewData['subtitle'] = 'Our stock';
-        $viewData['products'] = Product::all();
+        $viewData['products'] = $query->get();
+        $viewData["brands"] = $brands;
 
         return view('product.index')->with('viewData', $viewData);
     }
--- CustomKicks/resources/views/product/index.blade.php
+++ CustomKicks/resources/views/product/index.blade.php
@@ -6,6 +6,52 @@
 
 @section('content')
 
+
+<form method="GET" action="{{ route('product.index') }}" class="mb-4">
+    <div class="row">
+
+        <!-- Min Price -->
+        <div class="col-md-2">
+            <input type="number" name="min_price" class="form-control" placeholder="Minimum Price" 
+                value="{{ request()->input('min_price') }}">
+        </div>
+
+        <!-- Max Price -->
+        <div class="col-md-2">
+            <input type="number" name="max_price" class="form-control" placeholder="Maximum Price" 
+                value="{{ request()->input('max_price') }}">
+        </div>
+
+        <!-- Size -->
+        <div class="col-md-2">
+            <input type="float" name="size" class="form-control" placeholder="Size" 
+                value="{{ request()->input('size') }}">
+        </div>
+
+        <!-- Minimum Quantity -->
+        <div class="col-md-2">
+            <input type="number" name="quantity" class="form-control" placeholder="Quantity" 
+                value="{{ request()->input('quantity') }}">
+        </div>
+
+        <div class="col-md-2">
+            <select name="brand" class="form-control">
+                <option value="">Brands</option>
+                @foreach ($viewData["brands"] as $brand)
+                    <option value="{{ $brand }}" {{ request()->input('brand') == $brand ? 'selected' : '' }}>
+                        {{ $brand }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+
+        <!-- Filter botom -->
+        <div class="col-md-1">
+            <button type="submit" class="btn btn-primary">Filter</button>
+        </div>
+    </div>
+</form>
+
 <div class="row">
     @foreach ($viewData["products"] as $product)
         <div class="col-md-4 col-lg-3 mb-3">
